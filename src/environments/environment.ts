57de4d5f771d314bb44041e27ff52b6ce51cb4bf
Bartosz Zawadka (BAZ) <baz@kmd.dk>
2018-10-26 15:25:22 +0200

    Added keys to environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export let ENV = {
   "api": {
-    "endpoint": "http://localhost:8100/apibase/",
-    "key": "REPLACE_WITH_API_KEY"
+    "endpoint": "@@API_ENDPOINT@@",
+    "key": "@@API_KEY@@"
   },
   "dataRefreshInterval": 5000,
   "numberOfResults": 10
