ba854292823f34df6d5a1deb37bb0bde91568531
Bartek Zawadka <kontakt@bartoszzawadka.pl>
2018-10-27 10:36:36 +0200

    Fixed environments missing

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,8 @@
+export let ENV = {
+  "api": {
+    "endpoint": "http://localhost:8100/apibase/",
+    "key": "@@API_KEY@@"
+  },
+  "dataRefreshInterval": 5000,
+  "numberOfResults": 10
+};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export let ENV = {
+  "api": {
+    "endpoint": "@@API_ENDPOINT@@",
+    "key": "@@API_KEY@@"
+  },
+  "dataRefreshInterval": 5000,
+  "numberOfResults": 10
+};
